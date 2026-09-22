29f53bd79d6aa0092097473f3cca60e7fb6f609a
Khushi Sangal <sangalkhushi7@gmail.com>
2026-09-22 09:53:09 +0530

    Time: 3 ms (7.61%), Space: 13.3 MB (2.91%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        int temp=nums[0];
+       for(int i=0;i<n-1;i++)
+       {
+        if(nums[i]==nums[i+1])
+        {
+            nums[i]=nums[i]*2;
+            nums[i+1]=0;
+        }
+       }
+       int j = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        // Step 3: Fill remaining positions with zero
+        while(j < n) {
+            nums[j] = 0;
+            j++;
+        }
+       return nums;
+    }
+};
